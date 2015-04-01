2a22a99be933778d0d49e982aa32c5134f95e5e3
afrancis-heplat <afrancis.heplat@gmail.com>
2015-04-01 16:06:58 -0400

    Tested decuplet, adjusted correlator struct, added 16-component loop, got rid of threespinor-struct.

--- src/BARYONS/baryons.c
+++ src/BARYONS/baryons.c
@@ -14,7 +14,8 @@
 #include "read_propheader.h"   // for read_propheader()
 
 
-// This acts C*gamma_mu on a spinor, atm: Cg5
+// This acts C*gamma_mu on a spinor
+// Note: You need C*gamma_mu->source and (C*gamma_mu)^T->sink
 struct spinor
 Cgamma_mu( const struct spinor S, const int mu )
 {
@@ -83,8 +84,8 @@ Cgamma_mu( const struct spinor S, const int mu )
 				for( j = 0 ; j < NS ; j++ ) {
 					TSRC.D[0][j].C[c1][c2] = ( 1 + 0 * I ) * TSNK.D[3][j].C[c1][c2];
 					TSRC.D[1][j].C[c1][c2] = (-1 + 0 * I ) * TSNK.D[2][j].C[c1][c2];
-					TSRC.D[2][j].C[c1][c2] = ( 1 + 0 * I ) * TSNK.D[1][j].C[c1][c2];
-					TSRC.D[3][j].C[c1][c2] = (-1 + 0 * I ) * TSNK.D[0][j].C[c1][c2];
+					TSRC.D[2][j].C[c1][c2] = (-1 + 0 * I ) * TSNK.D[1][j].C[c1][c2];
+					TSRC.D[3][j].C[c1][c2] = ( 1 + 0 * I ) * TSNK.D[0][j].C[c1][c2];
 				}
 				break;
             case 1:
@@ -144,7 +145,7 @@ cross_color_trace( const struct spinor S ,
 {
 	int c1, c2, c3, i, j, d;
 	struct spinor TSRC;
-	struct threespinor T3SNK;
+	struct spinor T3SNK[3];
 
 	// Initialize the threespinor object
 	for( i = 0 ; i < NS ; i++ ) {
@@ -152,7 +153,7 @@ cross_color_trace( const struct spinor S ,
 			for( c1 = 0 ; c1 < NC ; c1++ ) {
 				for( c2 = 0 ; c2 < NC ; c2++ ) {
 					for( d = 0 ; d < NC ; d++ ) {
-						T3SNK.D[i][j].C[c1][c2][d] = 0.0;
+						T3SNK[d].D[i][j].C[c1][c2] = 0.0;
 					}
 				}
 			}
@@ -166,9 +167,9 @@ cross_color_trace( const struct spinor S ,
 				for( c2 = 0 ; c2 < NC ; c2++ ) {
 					// Dirac sink trace
 					for( d = 0 ; d < NS ; d++ ) {
-						T3SNK.D[i][j].C[c1][c2][0] += S.D[i][d].C[c1][1]*CgS.D[j][d].C[c2][2] - S.D[i][d].C[c1][2]*CgS.D[j][d].C[c2][1];
-						T3SNK.D[i][j].C[c1][c2][1] += S.D[i][d].C[c1][2]*CgS.D[j][d].C[c2][0] - S.D[i][d].C[c1][0]*CgS.D[j][d].C[c2][2];
-						T3SNK.D[i][j].C[c1][c2][2] += S.D[i][d].C[c1][0]*CgS.D[j][d].C[c2][1] - S.D[i][d].C[c1][1]*CgS.D[j][d].C[c2][0];
+						T3SNK[0].D[i][j].C[c1][c2] += S.D[i][d].C[c1][1]*CgS.D[j][d].C[c2][2] - S.D[i][d].C[c1][2]*CgS.D[j][d].C[c2][1];
+						T3SNK[1].D[i][j].C[c1][c2] += S.D[i][d].C[c1][2]*CgS.D[j][d].C[c2][0] - S.D[i][d].C[c1][0]*CgS.D[j][d].C[c2][2];
+						T3SNK[2].D[i][j].C[c1][c2] += S.D[i][d].C[c1][0]*CgS.D[j][d].C[c2][1] - S.D[i][d].C[c1][1]*CgS.D[j][d].C[c2][0];
 					}
 				}
 			}
@@ -179,9 +180,9 @@ cross_color_trace( const struct spinor S ,
 	for( i = 0 ; i < NS ; i++ ) {
         for( j = 0 ; j < NS ; j++ ) {
             for ( c3 = 0 ; c3 < 3 ; c3++ ){
-				TSRC.D[i][j].C[0][c3] = T3SNK.D[i][j].C[1][2][c3] - T3SNK.D[i][j].C[2][1][c3]; 
-				TSRC.D[i][j].C[1][c3] = T3SNK.D[i][j].C[2][0][c3] - T3SNK.D[i][j].C[0][2][c3]; 
-				TSRC.D[i][j].C[2][c3] = T3SNK.D[i][j].C[0][1][c3] - T3SNK.D[i][j].C[1][0][c3]; 
+				TSRC.D[i][j].C[0][c3] = T3SNK[c3].D[i][j].C[1][2] - T3SNK[c3].D[i][j].C[2][1]; 
+				TSRC.D[i][j].C[1][c3] = T3SNK[c3].D[i][j].C[2][0] - T3SNK[c3].D[i][j].C[0][2]; 
+				TSRC.D[i][j].C[2][c3] = T3SNK[c3].D[i][j].C[0][1] - T3SNK[c3].D[i][j].C[1][0]; 
 			}            
 		}
 	}
@@ -278,21 +279,18 @@ baryons_diagonal( struct propagator prop ,
 		  const char *outfile )
 {
 
-  // Define output correlators and some intermediate spinors
-  complex double term1, term2;
-  struct spinor CgS;	
-  struct spinor DiQ;	
-  struct correlator **Buds_corr = allocate_corrs( NSNS , NSNS ) ;
-  struct correlator **Buud_corr = allocate_corrs( NSNS , NSNS ) ;
-  struct correlator **Buuu_corr = allocate_corrs( NSNS , NSNS ) ;
+  // Define our output correlators, with 6 channels and 16 components
+  struct correlator **Buds_corr = allocate_corrs( 6 , NSNS ) ;
+  struct correlator **Buud_corr = allocate_corrs( 6 , NSNS ) ;
+  struct correlator **Buuu_corr = allocate_corrs( 6 , NSNS ) ;
 
   // and our spinors
   struct spinor *S1 ;
   if( posix_memalign( (void**)&S1 , 16 , 
 		      VOL3 * sizeof( struct spinor ) ) != 0 ) {
-      free_corrs( Buds_corr , NSNS , NSNS ) ; 
-      free_corrs( Buud_corr , NSNS , NSNS ) ; 
-      free_corrs( Buuu_corr , NSNS , NSNS ) ; 
+      free_corrs( Buds_corr , 6 , NSNS ) ; 
+      free_corrs( Buud_corr , 6 , NSNS ) ; 
+      free_corrs( Buuu_corr , 6 , NSNS ) ; 
 	  free( S1 ) ;
       printf( "[BARYONS] memalign failure \n" ) ;
       return FAILURE ;
@@ -304,9 +302,9 @@ baryons_diagonal( struct propagator prop ,
 
     // read in the file
     if( read_prop( prop , S1 ) == FAILURE ) {
-      free_corrs( Buds_corr , NSNS , NSNS ) ; 
-      free_corrs( Buud_corr , NSNS , NSNS ) ; 
-      free_corrs( Buuu_corr , NSNS , NSNS ) ; 
+      free_corrs( Buds_corr , 6 , NSNS ) ; 
+      free_corrs( Buud_corr , 6 , NSNS ) ; 
+      free_corrs( Buuu_corr , 6 , NSNS ) ; 
       free( S1 ) ;
       return FAILURE ;
     }
@@ -323,36 +321,50 @@ baryons_diagonal( struct propagator prop ,
     int GSRC = 0 ;
     // parallelise the furthest out loop
     #pragma omp parallel for private(GSRC)
-    for( GSRC = 5 ; GSRC < 6 ; GSRC++ ) {
-	int GSNK = GSRC;
+    for( GSRC = 0 ; GSRC < 6 ; GSRC++ ) {
 
-		register double complex Buds = 0.0 ;
-		register double complex Buud = 0.0 ;
-		register double complex Buuu = 0.0 ;
+		 // Define some intermediate spinors
+ 	 	complex double term1, term2;
+  		struct spinor CgS;	
+  		struct spinor DiQ;	
+  		register double complex Buds ;
+  		register double complex Buud ;
+  		register double complex Buuu ;
 
-		// loop spatial hypercube
-		int site ;
-		for( site = 0 ; site < VOL3 ; site++ ) {
-	
-			term1 = 0.0 ;
-			term2 = 0.0 ;
+		int odc=0;
+		int OD1;
+    	for( OD1 = 0 ; OD1 < 1 ; OD1++ ) {
 
-			CgS = reset_spinor();
-			DiQ = reset_spinor();
+			int OD2;
+    		for( OD2 = 0 ; OD2 < 1 ; OD2++ ) {
 
-			// Act C*gamma_mu on source and sink of spinor S1
-			CgS = Cgamma_mu( S1[ site ] , GSRC );
+				Buds = 0.0 ;
+				Buud = 0.0 ;
+				Buuu = 0.0 ;
 
-			// Cross color product and sink Dirac trace
-			DiQ = cross_color_trace( S1[ site ], CgS );
+				// loop spatial hypercube
+				int site ;
+				for( site = 0 ; site < VOL3 ; site++ ) {
+		
+				term1 = 0.0 ;
+				term2 = 0.0 ;
 
-			// Contract with the final propagator and trace out the source Dirac indices
-			// A polarization must still be picked for the two open Dirac indices offline
-			int dirac;
-			for( dirac = 0 ; dirac < NS ; dirac++ ){
-		 		term1 += baryon_contract( DiQ, S1[ site ], dirac , dirac , 0 , 0 );
-		 		term2 += baryon_contract( DiQ, S1[ site ], 0 , dirac , dirac , 0 );
-			}
+				CgS = reset_spinor();
+				DiQ = reset_spinor();
+
+				// Act C*gamma_mu on source and sink of spinor S1
+				CgS = Cgamma_mu( S1[ site ] , GSRC );
+
+				// Cross color product and sink Dirac trace
+				DiQ = cross_color_trace( S1[ site ], CgS );
+
+				// Contract with the final propagator and trace out the source Dirac indices
+				// A polarization must still be picked for the two open Dirac indices offline
+				int dirac;
+				for( dirac = 0 ; dirac < NS ; dirac++ ){
+		 			term1 += baryon_contract( DiQ, S1[ site ], dirac , dirac , OD1 , OD2 );
+		 			term2 += baryon_contract( DiQ, S1[ site ], OD1 , dirac , dirac , OD2 );
+				}
 
 //#ifdef DEBUG
 //	if( site < 2 ){
@@ -361,17 +373,27 @@ baryons_diagonal( struct propagator prop ,
 //	}
 //#endif
 
-			// Form the uds-, uud- and uuu-type baryons
-			Buds += term1 ;
-			Buud += term1 + term2 ;
-			Buuu += 2 * term1 + 4 * term2 ;
+				// Form the uds-, uud- and uuu-type baryons
+				Buds += term1 ;
+				Buud += term1 + term2 ;
+				Buuu += 2 * term1 + 4 * term2 ;
 
+//#ifdef DEBUG
+//	if( site == 0 ){
+//		printf("{Printing OD1=%d OD2=%d into index %d of corr files\n", OD1,OD2,odc);
+//	}
+//#endif
+				}
+
+				// Fill baryon correlator array
+				Buds_corr[ GSRC ][ odc ].C[ t ] = Buds ;
+				Buud_corr[ GSRC ][ odc ].C[ t ] = Buud ;
+				Buuu_corr[ GSRC ][ odc ].C[ t ] = Buuu ;
+			
+				// Move up the open Dirac counter
+				odc += 1 ;
+			}
 		}
-
-		// Fill baryon correlator array
-		Buds_corr[ GSRC ][ GSNK ].C[ t ] = Buds ;
-		Buud_corr[ GSRC ][ GSNK ].C[ t ] = Buud ;
-		Buuu_corr[ GSRC ][ GSNK ].C[ t ] = Buuu ;
 
     }
 
@@ -393,9 +415,9 @@ baryons_diagonal( struct propagator prop ,
   sprintf( outstr , "%s" , outfile ) ;
   //write_correlators( outstr , (const struct correlator**)Buds_corr ,NSNS , NSNS ) ;
 
-  free_corrs( Buds_corr , NSNS , NSNS ) ;
-  free_corrs( Buud_corr , NSNS , NSNS ) ;
-  free_corrs( Buuu_corr , NSNS , NSNS ) ;
+  free_corrs( Buds_corr , 6 , NSNS ) ;
+  free_corrs( Buud_corr , 6 , NSNS ) ;
+  free_corrs( Buuu_corr , 6 , NSNS ) ;
 
   free( S1 ) ;
 
