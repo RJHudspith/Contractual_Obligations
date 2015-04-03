fdbe776cf5380f3194df3bee8321de3432b74afb
RJHudspith <renwick.james.hudspith@gmail.com>
2015-04-03 03:05:48 -0400

    implemented a master-slave style IO so that a thread attempts to read the next timeslice worth of prop whilst the contractions are done in parallel, significant speed improvement seen

--- src/MEAS/mesons.c
+++ src/MEAS/mesons.c
@@ -45,19 +45,28 @@ mesons_diagonal( struct propagator prop ,
     wwcorr = allocate_corrs( NSNS , NSNS ) ;
   }
 
+  struct spinor *S1f ;
+  if( posix_memalign( (void**)&S1f , 16 , 
+		      VOL3 * sizeof( struct spinor ) ) != 0 ) {
+    free( S1 ) ; free( S1f ) ; free( GAMMAS ) ;
+    printf( "[MESONS] memalign failure \n" ) ;
+    return FAILURE ;
+  }
+
+  // initially read in a timeslice
+  if( read_prop( prop , S1 ) == FAILURE ) {
+    free_corrs( wlcorr , NSNS , NSNS ) ; 
+    if( prop.source == WALL ) {
+      free_corrs( wwcorr , NSNS , NSNS ) ;
+    }
+    free( GAMMAS ) ; free( S1 ) ;
+    return FAILURE ;
+  }
+
+  
   int t ;
   // Time slice loop 
   for( t = 0 ; t < L0 ; t++ ) {
-
-    // read in the file
-    if( read_prop( prop , S1 ) == FAILURE ) {
-      free_corrs( wlcorr , NSNS , NSNS ) ; 
-      if( prop.source == WALL ) {
-	free_corrs( wwcorr , NSNS , NSNS ) ;
-      }
-      free( GAMMAS ) ; free( S1 ) ;
-      return FAILURE ;
-    }
     
     // single sum
     struct spinor SUM1 ;
@@ -65,16 +74,33 @@ mesons_diagonal( struct propagator prop ,
       sumprop( &SUM1 , S1 ) ;
     }
 
-    int GSRC = 0 ;
-    // parallelise the furthest out loop
-    #pragma omp parallel for private(GSRC)
-    for( GSRC = 0 ; GSRC < NSNS ; GSRC++ ) { // NSNS
+    // above should be non-blocking allowing us to play here
+    int GSGK = 0 ;
+    #pragma omp parallel
+    {
+      #pragma omp master
+      {
+	if( t < ( L0 - 1 ) ) {
+	  if( read_prop( prop , S1f ) == FAILURE ) {
+	    free_corrs( wlcorr , NSNS , NSNS ) ; 
+	    if( prop.source == WALL ) {
+	      free_corrs( wwcorr , NSNS , NSNS ) ;
+	    }
+	    free( GAMMAS ) ; free( S1f ) ; free( S1 ) ;
+	  }
+	  //
+	}
+      }
 
-      int GSNK ;
-      for( GSNK = 0 ; GSNK < NSNS ; GSNK++ ) {// NSNS
-
+      // parallelise the furthest out loop :: flatten the gammas
+      #pragma omp for private(GSGK) schedule(dynamic)
+      for( GSGK = 0 ; GSGK < ( NSNS*NSNS ) ; GSGK++ ) {
+	
+	const int GSRC = GSGK / NSNS ;
+	const int GSNK = GSGK % NSNS ;
+	
 	register double complex sum = 0.0 ;
-
+	
 	// loop spatial hypercube
 	int site ;
 	for( site = 0 ; site < VOL3 ; site++ ) {
@@ -82,10 +108,10 @@ mesons_diagonal( struct propagator prop ,
 				 GAMMAS[ GSRC ] , S1[ site ] ,
 				 GAMMAS[ GAMMA_5 ] ) ;
 	}
-
+	
 	// normal wall-local meson correlator
 	wlcorr[ GSRC ][ GSNK ].C[ t ] = sum ;
-
+	
 	if( prop.source == WALL ) {
 	  // correlator computed just out of the summed walls
 	  wwcorr[ GSRC ][ GSNK ].C[ t ] =		\
@@ -95,6 +121,13 @@ mesons_diagonal( struct propagator prop ,
 	}
 	//
       }
+    }
+
+    // copy S1f into S1
+    int i ;
+#pragma omp parallel for private(i)
+    for( i = 0 ; i < LCU ; i++ ) {
+      memcpy( &S1[i] , &S1f[i] , sizeof( struct spinor ) ) ;
     }
 
     // status of the computation
@@ -132,6 +165,7 @@ mesons_diagonal( struct propagator prop ,
 
   // free our spinor
   free( S1 ) ;
+  free( S1f ) ; // free the copy
 
   // rewind file and read header again
   rewind( prop.file ) ; read_propheader( &prop ) ;
@@ -159,17 +193,31 @@ mesons_offdiagonal( struct propagator prop1 ,
   }
 
   // and our spinors
-  struct spinor *S1 ;
+  struct spinor *S1 = NULL ;
   if(  posix_memalign( (void**)&S1 , 16 , 
 		       VOL3 * sizeof( struct spinor ) ) != 0 ) {
     free( S1 ) ; free( GAMMAS ) ;
     printf( "[MESONS] memalign failure \n" ) ;
     return FAILURE ;
   }
-  struct spinor *S2 ;
+  struct spinor *S2 = NULL ;
   if( posix_memalign( (void**)&S2 , 16 , 
 		      VOL3 * sizeof( struct spinor ) ) != 0 ) {
     free( S1 ) ; free( S2 ) ; free( GAMMAS ) ;
+    printf( "[MESONS] memalign failure \n" ) ;
+    return FAILURE ;
+  }
+  struct spinor *S1f = NULL ;
+  if(  posix_memalign( (void**)&S1f , 16 , 
+		       VOL3 * sizeof( struct spinor ) ) != 0 ) {
+    free( S1 ) ; free( GAMMAS ) ;
+    printf( "[MESONS] memalign failure \n" ) ;
+    return FAILURE ;
+  }
+  struct spinor *S2f = NULL ;
+  if( posix_memalign( (void**)&S2f , 16 , 
+		      VOL3 * sizeof( struct spinor ) ) != 0 ) {
+    free( S1 ) ; free( S2 ) ; free( S1f ) ; free( S2f ) ; free( GAMMAS ) ;
     printf( "[MESONS] memalign failure \n" ) ;
     return FAILURE ;
   }
@@ -179,23 +227,23 @@ mesons_offdiagonal( struct propagator prop1 ,
   struct correlator **wwcorr = NULL ;
   
   if( prop1.source == WALL || prop2.source == WALL ) {
-    allocate_corrs( NSNS , NSNS ) ;
+    wwcorr = allocate_corrs( NSNS , NSNS ) ;
+  }
+
+  // read in the files
+  if( read_prop( prop1 , S1 ) == FAILURE ||
+      read_prop( prop2 , S2 ) == FAILURE ) {
+    free_corrs( wlcorr , NSNS , NSNS ) ; 
+    if( prop1.source == WALL || prop2.source == WALL ) {
+      free_corrs( wwcorr , NSNS , NSNS ) ;
+    }
+    free( GAMMAS ) ; free( S1 ) ; free( S2 ) ;
+    return FAILURE ;
   }
 
   int t ;
   // Time slice loop 
   for( t = 0 ; t < L0 ; t++ ) {
-
-    // read in the file
-    if( read_prop( prop1 , S1 ) == FAILURE ||
-	read_prop( prop2 , S2 ) == FAILURE ) {
-      free_corrs( wlcorr , NSNS , NSNS ) ; 
-      if( prop1.source == WALL || prop2.source == WALL ) {
-	free_corrs( wwcorr , NSNS , NSNS ) ;
-      }
-      free( GAMMAS ) ; free( S1 ) ; free( S2 ) ;
-      return FAILURE ;
-    }
 
     // if we are doing nonrel-chiral mesons we switch chiral to nrel
     if( prop1.basis == CHIRAL && prop2.basis == NREL ) {
@@ -211,16 +259,33 @@ mesons_offdiagonal( struct propagator prop1 ,
       sumprop( &SUM2 , S2 ) ;
     }
 
-    int GSRC = 0 ;
-    // parallelise the furthest out loop
-    #pragma omp parallel for private(GSRC)
-    for( GSRC = 0 ; GSRC < NSNS ; GSRC++ ) {
+    int GSGK = 0 ;
+    #pragma omp parallel
+    {
+      // two threads for IO :: TODO, make this more safe
+      if( t < ( L0 - 1 ) ) {
+        #pragma omp master
+	{
+	  if( read_prop( prop1 , S1f ) == FAILURE ) {
+	  }
+	}
+        #pragma omp single nowait
+	{
+	  if( read_prop( prop2 , S2f ) == FAILURE ) {
+	  }
+	}
+      }
 
-      int GSNK ;
-      for( GSNK = 0 ; GSNK < NSNS ; GSNK++ ) {
+      // parallelise the furthest out loop
+#pragma omp for private(GSGK) schedule(dynamic)
+      for( GSGK = 0 ; GSGK < ( NSNS * NSNS ) ; GSGK++ ) {
+	
+	const int GSRC = GSGK / NSNS ;
+	const int GSNK = GSGK % NSNS ;
 
+	// accumulate sum
 	register double complex sum = 0.0 ;
-
+	
 	// loop spatial hypercube
 	int site ;
 	for( site = 0 ; site < VOL3 ; site++ ) {
@@ -243,6 +308,14 @@ mesons_offdiagonal( struct propagator prop1 ,
       }
     }
 
+    // and swap back
+    int i ;
+    #pragma omp parallel for private(i)
+    for( i = 0 ; i < LCU ; i++ ) {
+      memcpy( &S1[i] , &S1f[i] , sizeof( struct spinor ) ) ;
+      memcpy( &S2[i] , &S2f[i] , sizeof( struct spinor ) ) ;
+    }
+
     // status of the computation
     printf("\r[MESONS] done %.f %%", (t+1)/((L0)/100.) ) ; 
     fflush( stdout ) ;
@@ -258,7 +331,7 @@ mesons_offdiagonal( struct propagator prop1 ,
 
   // outputs
   char outstr[ 256 ] ;
-  sprintf( outstr , "%s.wl" , outfile ) ;
+  sprintf( outstr , "%s" , outfile ) ;
   write_correlators( outstr , (const struct correlator**)wlcorr ,
 		     NSNS , NSNS ) ;
 
@@ -276,7 +349,9 @@ mesons_offdiagonal( struct propagator prop1 ,
 
   // free our spinors
   free( S1 ) ;
+  free( S1f ) ;
   free( S2 ) ;
+  free( S2f ) ;
 
   // rewind file and read header again
   rewind( prop1.file ) ; read_propheader( &prop1 ) ;
