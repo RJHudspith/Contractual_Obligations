c0f37da80a11611d898154732576dc5c3b349986
Renwick James Hudspith <renwick.james.hudspith@gmail.com>
2018-10-02 14:26:40 -0400

    I don't really get why gcc-7 complained about Fmunu_halfspinor, but this function is really called so rolling back to a simpler version is fine.

--- src/LINALG/halfspinor_ops_SSE.c
+++ src/LINALG/halfspinor_ops_SSE.c
@@ -12,80 +12,6 @@
   #include "AVX_OPS.h"
 #endif
 
-// pA = pB.pC where pB is hermitian and pC is an ordinary 3x3 matrix
-static inline void
-herm_multab( __m128d *__restrict pA ,
-	     const __m128d *__restrict pB ,
-	     const __m128d *__restrict pC )
-{
-#if NC == 3 
-  register const __m128d reA = _mm_movedup_pd( *( pB + 0 ) ) ;
-  register const __m128d reD = _mm_movedup_pd( *( pB + 4 ) ) ;
-  register const __m128d reF = SSE_FLIP( _mm_add_pd( reA , reD ) ) ;
-  // first row
-  *pA = _mm_add_pd( _mm_mul_pd( reA , *( pC + 0 ) ) , 
-		    SSE2_MUL( *( pB + 1 ) , *( pC + 3 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 2 ) , *( pC + 6 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( _mm_mul_pd( reA , *( pC + 1 ) ) , 
-		    SSE2_MUL( *( pB + 1 ) , *( pC + 4 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 2 ) , *( pC + 7 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( _mm_mul_pd( reA , *( pC + 2 ) ) , 
-		    SSE2_MUL( *( pB + 1 ) , *( pC + 5 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 2 ) , *( pC + 8 ) ) ) ; pA++ ;
-  // sepCond row
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 1 ) , *( pC + 0 ) ) , 
-		    _mm_mul_pd( reD , *( pC + 3 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 5 ) , *( pC + 6 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 1 ) , *( pC + 1 ) ) , 
-		    _mm_mul_pd( reD , *( pC + 4 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 5 ) , *( pC + 7 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 1 ) , *( pC + 2 ) ) , 
-		    _mm_mul_pd( reD , *( pC + 5 ) ) ) ;
-  *pA = _mm_add_pd( *pA , SSE2_MUL( *( pB + 5 ) , *( pC + 8 ) ) ) ; pA++ ;
-  // third row
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 2 ) , *( pC + 0 ) ) , 
-		    SSE2_MULCONJ( *( pB + 5 ) , *( pC + 3 ) ) ) ;
-  *pA = _mm_add_pd( *pA , _mm_mul_pd( reF , *( pC + 6 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 2 ) , *( pC + 1 ) ) , 
-		    SSE2_MULCONJ( *( pB + 5 ) , *( pC + 4 ) ) ) ;
-  *pA = _mm_add_pd( *pA , _mm_mul_pd( reF , *( pC + 7 ) ) ) ; pA++ ;
-  *pA = _mm_add_pd( SSE2_MULCONJ( *( pB + 2 ) , *( pC + 2 ) ) , 
-		    SSE2_MULCONJ( *( pB + 5 ) , *( pC + 5 ) ) ) ;
-  *pA = _mm_add_pd( *pA , _mm_mul_pd( reF , *( pC + 8 ) ) ) ;
-#else
-  multab( pA , pB , pC ) ;
-#endif
-}
-
-// atomically add halfspinors a += b
-void
-add_halfspinor( struct halfspinor *a ,
-		const struct halfspinor b )
-{
-  __m128d *pA = (__m128d*)a -> D ;
-  const __m128d *pB = (const __m128d*)b.D ;
-  size_t i ;
-  for( i = 0 ; i < NCNC ; i++ ) {
-    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
-    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
-    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
-    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
-  }
-  return ;
-}
-
-void
-Fmunu_halfspinor( struct halfspinor *a ,
-		  const double complex *b ,
-		  const struct halfspinor c )
-{
-  herm_multab( (void*)a->D[0] , (void*)b , (void*)c.D[0] ) ;
-  herm_multab( (void*)a->D[1] , (void*)b , (void*)c.D[1] ) ;
-  herm_multab( (void*)a->D[2] , (void*)b , (void*)c.D[2] ) ;
-  herm_multab( (void*)a->D[3] , (void*)b , (void*)c.D[3] ) ;
-  return ;
-}
-
 #if NC==3
 #define inline_su3(pA,pB,pC) \
   *pA = _mm_add_pd( SSE2_MUL( *( pB + 0 ) , *( pC + 0 ) ) ,		\
@@ -125,6 +51,35 @@ Fmunu_halfspinor( struct halfspinor *a ,
 				SSE2_MUL( *( pB + 8 ) , *( pC + 8 ) ) ) ) ; \
   pA++ ;								
 #endif
+
+// atomically add halfspinors a += b
+void
+add_halfspinor( struct halfspinor *a ,
+		const struct halfspinor b )
+{
+  __m128d *pA = (__m128d*)a -> D ;
+  const __m128d *pB = (const __m128d*)b.D ;
+  size_t i ;
+  for( i = 0 ; i < NCNC ; i++ ) {
+    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
+    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
+    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
+    *pA = _mm_add_pd( *pA , *pB ) ; pA++ ; pB++ ;
+  }
+  return ;
+}
+
+void
+Fmunu_halfspinor( struct halfspinor *a ,
+		  const double complex *b ,
+		  const struct halfspinor c )
+{
+  multab( (void*)a->D[0] , (void*)b , (void*)c.D[0] ) ;
+  multab( (void*)a->D[1] , (void*)b , (void*)c.D[1] ) ;
+  multab( (void*)a->D[2] , (void*)b , (void*)c.D[2] ) ;
+  multab( (void*)a->D[3] , (void*)b , (void*)c.D[3] ) ;
+  return ;
+}
 
 // this thing gets called so often I really spent some time optimising it:
 // the function calls _mm256d_addsub_pd a lot and _mmm256_setr_pd
