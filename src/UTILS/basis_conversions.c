74b05cacc9373b3c32cd3cb1d25c4dcbd120d6b6
Renwick James Hudspith <renwick.james.hudspith@gmail.com>
2019-05-10 19:25:48 +0200

    Updated the basis conversions and removed the TODO on it.

--- src/UTILS/basis_conversions.c
+++ src/UTILS/basis_conversions.c
@@ -1,44 +1,108 @@
 /**
    @file basis_conversions.c
-   @brief conversions between gamma bases
-
-   TODO :: cache coherency - J
+   @brief conversions between our chiral and the NREL (Dirac-Pauli) basis 
  */
 #include "common.h"
 
-// convert chiral to Nrel
+#ifdef __SSE2__
+  #include <immintrin.h>
+#endif
+
+// add four color matrices
+static void
+add_colors( void *R ,
+	    const void *A ,
+	    const void *B ,
+	    const void *C ,
+	    const void *D )
+{
+#ifdef __SSE2__
+  __m128d *pR = (__m128d*)R ;
+  const __m128d *pA = (const __m128d*)A , *pB = (const __m128d*)B ;
+  const __m128d *pC = (const __m128d*)C , *pD = (const __m128d*)D ;
+  register const __m128d onehalf = _mm_setr_pd( 0.5 , 0.5 ) ;
+  size_t c ;
+  for( c = 0 ; c < NCNC ; c++ ) {
+    *pR = _mm_mul_pd( onehalf , _mm_add_pd( _mm_add_pd( *pA , *pB ) ,
+					    _mm_add_pd( *pC , *pD ) ) ) ;
+    pR++ ; pA++ ; pB++ ; pC++ ; pD++ ;
+  }
+#else
+  double *pR = (double*)R ;
+  const double complex *pA = (const double complex*)A ;
+  const double complex *pB = (const double complex*)B ;
+  const double complex *pC = (const double complex*)C ;
+  const double complex *pD = (const double complex*)D ;
+  register const double onehalf = 0.5 ;
+  size_t c ;
+  for( c = 0 ; c < NCNC ; c++ ) {
+    *pR = onehalf*( *pA + *pB + *pC + *pD ) ;
+    pR++ ; pA++ ; pB++ ; pC++ ; pD++ ;
+  }
+#endif
+}
+
+// do (A+B)-(C+D) for color matrices 
+static void
+sub_colors( void *R ,
+	    const void *A ,
+	    const void *B ,
+	    const void *C ,
+	    const void *D )
+{
+#ifdef __SSE2__
+  __m128d *pR = (__m128d*)R ;
+  const __m128d *pA = (const __m128d*)A , *pB = (const __m128d*)B ;
+  const __m128d *pC = (const __m128d*)C , *pD = (const __m128d*)D ;
+  register const __m128d onehalf = _mm_setr_pd( 0.5 , 0.5 ) ;
+  size_t c ;
+  for( c = 0 ; c < NCNC ; c++ ) {
+    *pR = _mm_mul_pd( onehalf , _mm_sub_pd( _mm_add_pd( *pA , *pB ) ,
+					    _mm_add_pd( *pC , *pD ) ) ) ;
+    pR++ ; pA++ ; pB++ ; pC++ ; pD++ ;
+  }
+#else
+  double *pR = (double*)res ;
+  const double complex *pA = (const double complex*)A ;
+  const double complex *pB = (const double complex*)B ;
+  const double complex *pC = (const double complex*)C ;
+  const double complex *pD = (const double complex*)D ;
+  register const double onehalf = 0.5 ;
+  size_t c ;
+  for( c = 0 ; c < NCNC ; c++ ) {
+    *pR = onehalf*( *pA + *pB - ( *pC + *pD ) ) ;
+    pR++ ; pA++ ; pB++ ; pC++ ; pD++ ;
+  }
+#endif
+}
+
+// is rotated from the eigenvectors of gamma_t
 void
 chiral_to_nrel( struct spinor *S )
 {
   struct spinor P = *S ;
-  size_t c1c2 ;
-  for( c1c2 = 0 ; c1c2 < NCNC ; c1c2++ ) {
-    const size_t c1 = c1c2 / NC ;
-    const size_t c2 = c1c2 % NC ;
-    // reverted right back to the previous version
-    // first row
-    S -> D[0][0].C[c1][c2] = 0.5 * ( +P.D[0][0].C[c1][c2] + P.D[2][0].C[c1][c2] + P.D[0][2].C[c1][c2] + P.D[2][2].C[c1][c2] ) ;
-    S -> D[0][1].C[c1][c2] = 0.5 * ( +P.D[0][1].C[c1][c2] + P.D[2][1].C[c1][c2] + P.D[0][3].C[c1][c2] + P.D[2][3].C[c1][c2] ) ;
-    S -> D[0][2].C[c1][c2] = 0.5 * ( -P.D[0][0].C[c1][c2] - P.D[2][0].C[c1][c2] + P.D[0][2].C[c1][c2] + P.D[2][2].C[c1][c2] ) ;
-    S -> D[0][3].C[c1][c2] = 0.5 * ( -P.D[0][1].C[c1][c2] - P.D[2][1].C[c1][c2] + P.D[0][3].C[c1][c2] + P.D[2][3].C[c1][c2] ) ;
-    // second row
-    S -> D[1][0].C[c1][c2] = 0.5 * ( +P.D[1][0].C[c1][c2] + P.D[3][0].C[c1][c2] + P.D[1][2].C[c1][c2] + P.D[3][2].C[c1][c2] ) ;
-    S -> D[1][1].C[c1][c2] = 0.5 * ( +P.D[1][1].C[c1][c2] + P.D[3][1].C[c1][c2] + P.D[1][3].C[c1][c2] + P.D[3][3].C[c1][c2] ) ;
-    S -> D[1][2].C[c1][c2] = 0.5 * ( -P.D[1][0].C[c1][c2] - P.D[3][0].C[c1][c2] + P.D[1][2].C[c1][c2] + P.D[3][2].C[c1][c2] ) ;
-    S -> D[1][3].C[c1][c2] = 0.5 * ( -P.D[1][1].C[c1][c2] - P.D[3][1].C[c1][c2] + P.D[1][3].C[c1][c2] + P.D[3][3].C[c1][c2] ) ;
-    // third row
-    S -> D[2][0].C[c1][c2] = 0.5 * ( -P.D[0][0].C[c1][c2] + P.D[2][0].C[c1][c2] - P.D[0][2].C[c1][c2] + P.D[2][2].C[c1][c2] ) ;
-    S -> D[2][1].C[c1][c2] = 0.5 * ( -P.D[0][1].C[c1][c2] + P.D[2][1].C[c1][c2] - P.D[0][3].C[c1][c2] + P.D[2][3].C[c1][c2] ) ;
-    S -> D[2][2].C[c1][c2] = 0.5 * ( +P.D[0][0].C[c1][c2] - P.D[2][0].C[c1][c2] - P.D[0][2].C[c1][c2] + P.D[2][2].C[c1][c2] ) ;
-    S -> D[2][3].C[c1][c2] = 0.5 * ( +P.D[0][1].C[c1][c2] - P.D[2][1].C[c1][c2] - P.D[0][3].C[c1][c2] + P.D[2][3].C[c1][c2] ) ;
-    // fourth row
-    S -> D[3][0].C[c1][c2] = 0.5 * ( -P.D[1][0].C[c1][c2] + P.D[3][0].C[c1][c2] - P.D[1][2].C[c1][c2] + P.D[3][2].C[c1][c2] ) ;
-    S -> D[3][1].C[c1][c2] = 0.5 * ( -P.D[1][1].C[c1][c2] + P.D[3][1].C[c1][c2] - P.D[1][3].C[c1][c2] + P.D[3][3].C[c1][c2] ) ;
-    S -> D[3][2].C[c1][c2] = 0.5 * ( +P.D[1][0].C[c1][c2] - P.D[3][0].C[c1][c2] - P.D[1][2].C[c1][c2] + P.D[3][2].C[c1][c2] ) ;
-    S -> D[3][3].C[c1][c2] = 0.5 * ( +P.D[1][1].C[c1][c2] - P.D[3][1].C[c1][c2] - P.D[1][3].C[c1][c2] + P.D[3][3].C[c1][c2] ) ;	
-  }
+  // first row
+  add_colors( S->D[0][0].C , P.D[0][0].C , P.D[2][0].C , P.D[0][2].C , P.D[2][2].C ) ;
+  add_colors( S->D[0][1].C , P.D[0][1].C , P.D[2][1].C , P.D[0][3].C , P.D[2][3].C ) ;
+  sub_colors( S->D[0][2].C , P.D[0][2].C , P.D[2][2].C , P.D[0][0].C , P.D[2][0].C ) ;
+  sub_colors( S->D[0][3].C , P.D[0][3].C , P.D[2][3].C , P.D[0][1].C , P.D[2][1].C ) ;
+  // second row
+  add_colors( S->D[1][0].C , P.D[1][0].C , P.D[3][0].C , P.D[1][2].C , P.D[3][2].C ) ;
+  add_colors( S->D[1][1].C , P.D[1][1].C , P.D[3][1].C , P.D[1][3].C , P.D[3][3].C ) ;
+  sub_colors( S->D[1][2].C , P.D[1][2].C , P.D[3][2].C , P.D[1][0].C , P.D[3][0].C ) ;
+  sub_colors( S->D[1][3].C , P.D[1][3].C , P.D[3][3].C , P.D[1][1].C , P.D[3][1].C ) ;
+  // third row
+  sub_colors( S->D[2][0].C , P.D[2][0].C , P.D[2][2].C , P.D[0][2].C , P.D[0][0].C ) ;
+  sub_colors( S->D[2][1].C , P.D[2][1].C , P.D[2][3].C , P.D[0][3].C , P.D[0][1].C ) ;
+  sub_colors( S->D[2][2].C , P.D[0][0].C , P.D[2][2].C , P.D[2][0].C , P.D[0][2].C ) ;
+  sub_colors( S->D[2][3].C , P.D[0][1].C , P.D[2][3].C , P.D[2][1].C , P.D[0][3].C ) ;
+  // fourth row
+  sub_colors( S->D[3][0].C , P.D[3][0].C , P.D[3][2].C , P.D[1][0].C , P.D[1][2].C ) ;
+  sub_colors( S->D[3][1].C , P.D[3][1].C , P.D[3][3].C , P.D[1][1].C , P.D[1][3].C ) ;
+  sub_colors( S->D[3][2].C , P.D[1][0].C , P.D[3][2].C , P.D[3][0].C , P.D[1][2].C ) ;
+  sub_colors( S->D[3][3].C , P.D[1][1].C , P.D[3][3].C , P.D[3][1].C , P.D[1][3].C ) ;	
   return ;
-}  
+}
 
 // rotate a timeslice
 static void
